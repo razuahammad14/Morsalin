4a75e463091d7da6fa464e8d4ca57d7c14334718
Molla <Greenchili@razus-imac.local>
2016-06-04 19:51:58 +0600

    Initial Commit

---
+++ Morsalin/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Morsalin
+//
+//  Created by Razu Ahammad Molla on 6/4/16.
+//  Copyright © 2016 Razu Ahammad Molla. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
